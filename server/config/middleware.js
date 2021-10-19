6565f0255f931fa81c79544c1d3cab38b04e5628
Peter Akaliro <pin4eva@hotmail.com>
2021-10-19 22:01:02 +0100

    init 3

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,4 +1,4 @@
-const origin = ["http://localhost:8000","http://localhost:3000","http://localhost:3001","http://143.198.164.151","https://ariseigalaonline.com","https://server.ariseigalaonline.com","https://dev.ariseigalaonline.com"]
+const origin = ["http://localhost:8000","http://localhost:3000","http://localhost:3001","http://143.198.164.151","http://143.198.164.151:8000","https://ariseigalaonline.com","https://server.ariseigalaonline.com","https://dev.ariseigalaonline.com"]
 
 module.exports =({env})=> ({
   settings: {
