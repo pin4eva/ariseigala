d4567d4a8c00b34de70bbad8e87e53da81e5b6be
Peter Akaliro <pin4eva@hotmail.com>
2021-10-20 00:25:13 +0100

    init 5

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,12 +1,9 @@
-// const SERVER_URI = "hela-server-d8vwy.ondigitalocean.app";
-export const SERVER_URI = process.env.SERVER_URI || "http://localhost:8000";
+export const SERVER_URI =
+	process.env.NODE_ENV === "production"
+		? "https://server.ariseigalaonline.com/"
+		: "http://localhost:8000";
 
 export const TOKEN_NAME = "__ar";
-
-export const HTTP_URI =
-	process.env.NODE_ENV === "production"
-		? "https://araiseigalaonline.com"
-		: "http://localhost:8000";
 
 export const WS_URI =
 	process.env.NODE_ENV === "production"
