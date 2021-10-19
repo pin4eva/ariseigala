7ffdbb3fa05e8da941040ce836af39f0656645c1
Peter Akaliro <pin4eva@hotmail.com>
2021-10-20 00:37:55 +0100

    made changes to server

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -5,6 +5,8 @@ export const SERVER_URI =
 
 export const TOKEN_NAME = "__ar";
 
+export const HTTP_URI = SERVER_URI;
+
 export const WS_URI =
 	process.env.NODE_ENV === "production"
 		? `wss://server.ariseigalaonline.com`
--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,4 +1,8 @@
-const origin = ["http://localhost:8000","http://localhost:3000","http://localhost:3001","http://143.198.164.151","http://143.198.164.151:8000","https://ariseigalaonline.com","https://ariseigala.vercel.app","https://server.ariseigalaonline.com","https://dev.ariseigalaonline.com"]
+const devOrigins = ["http://localhost:8000", "http://localhost:3000", "http://localhost:3001"]
+
+const prodOrigins = ["http://143.198.164.151", "http://143.198.164.151:8000", "https://ariseigalaonline.com", "https://ariseigala.vercel.app", "https://server.ariseigalaonline.com", "https://dev.ariseigalaonline.com"]
+
+const origin = process.env.NODE_ENV === "production" ? prodOrigins : devOrigins;
 
 module.exports =({env})=> ({
   settings: {
