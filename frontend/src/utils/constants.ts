28d896558509f7e7a300fb04b329b6aa30124967
Peter Akaliro <pin4eva@hotmail.com>
2021-10-20 00:27:13 +0100

    init dev

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,11 +1,11 @@
 export const SERVER_URI =
 	process.env.NODE_ENV === "production"
-		? "https://server.ariseigalaonline.com/"
+		? "https://server.ariseigalaonline.com"
 		: "http://localhost:8000";
 
 export const TOKEN_NAME = "__ar";
 
 export const WS_URI =
 	process.env.NODE_ENV === "production"
-		? `wss://${SERVER_URI}`
+		? `wss://server.ariseigalaonline.com`
 		: "ws://localhost:8000";
