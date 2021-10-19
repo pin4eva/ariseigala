91588eef7e8d14f7beec12bc7a7bf7825af21248
Peter Akaliro <pin4eva@hotmail.com>
2021-10-19 21:19:59 +0100

    init 2

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,11 +1,10 @@
-const prodOrigins = ["http://195.110.59.91","http://server.edfhr.org","https://server.edfhr.org",]
-const devOrigins = ["http://localhost:3000","http://localhost:3001","http://localhost:8000",]
+const origin = ["http://localhost:8000","http://localhost:3000","http://localhost:3001","http://143.198.164.151","https://ariseigalaonline.com","https://server.ariseigalaonline.com","https://dev.ariseigalaonline.com"]
 
 module.exports =({env})=> ({
   settings: {
     cors: {
       enable: true,
-      origin: env("NODE_ENV")==="production"? prodOrigins : devOrigins
+      origin
     }
   }
 })
